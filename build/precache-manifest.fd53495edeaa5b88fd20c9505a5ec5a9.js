c54394eb551e6398798698a95fd0f84cde1a4514
mdmusaib <musaibkm@gmail.com>
2020-06-07 13:07:29 +0530

    ui server changes

---
+++ build/precache-manifest.fd53495edeaa5b88fd20c9505a5ec5a9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6b4874116bfaec56215956782d7a2d22",
+    "url": "/index.html"
+  },
+  {
+    "revision": "54db8425f5b9f41ebf41",
+    "url": "/static/css/2.d7262850.chunk.css"
+  },
+  {
+    "revision": "0bce34372b0fa39c5182",
+    "url": "/static/css/main.86c47784.chunk.css"
+  },
+  {
+    "revision": "54db8425f5b9f41ebf41",
+    "url": "/static/js/2.6f879aa8.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.6f879aa8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0bce34372b0fa39c5182",
+    "url": "/static/js/main.33a11762.chunk.js"
+  },
+  {
+    "revision": "98d5d951b7c20af2c290",
+    "url": "/static/js/runtime-main.ce07403c.js"
+  }
+]);
