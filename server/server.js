9245bc63783dec8e588b3b0b97f3398572668dd6
Musaib <musaibkm@gmail.com>
2020-06-07 13:02:27 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -1,8 +1,31 @@
-const http = require('http');
-const app = require('./app');
+// const http = require('http');
+// const app = require('./app');
 
-const port = 9000;
+// const port = 9000;
 
-const server = http.createServer(app);
+// const server = http.createServer(app);
 
-server.listen(port);
+// server.listen(port);
+
+const http = require("http");
+const path = require("path");
+const express = require("express");
+const app = express();
+
+const serverApp = require("./app");
+const serverPort = 9000;
+
+const publicPath = path.join(__dirname, "..", "build");
+const port = process.env.PORT || 3000;
+app.use(express.static(publicPath));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(publicPath, "index.html"));
+});
+
+app.listen(port, () => {
+  console.log("ui is ready");
+});
+
+const server = http.createServer(serverApp);
+server.listen(serverPort);
